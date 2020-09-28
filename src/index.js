854f2913c86337e0f212929dafc328a2c01f36b3
Pietro Pereira <piietro.pereira@hotmail.com>
2020-09-28 18:49:02 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './main/Calculator'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Calculadora</h1>
+    <Calculator></Calculator>
+    
+  </div>,
   document.getElementById('root')
 );
 
